9df527087c7fa675f6ddce7d2464c56bb34c5135
Shiranka Miskin <shiranka.miskin@gmail.com>
2017-07-14 13:25:28 -0400

    Progress update + SA

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import copy
 import math
 import random
 
+
 w = 8
 h = 7
 num_districts = 7
@@ -11,12 +12,19 @@ num_districts = 7
 toXY = lambda i: (i % w, i / w)
 toI = lambda x, y: y*w + x
 
-populations = [int(random.random()*100 + 1) for i in range(w*h)]
+#populations = [int(random.random()*100 + 1) for i in range(w*h)]
+populations = [1 for i in range(w*h)]
 areas = [1 for i in range(w*h)]
 
-# A solution is a mapping from cell index to district number
-# This initial solution distributes districts evenly across rows
-initial_solution = [int(i / (math.ceil((float(w*h))/num_districts))) for i in range(w*h)]
+# A solution is a mapping from cell index to district number.  Since ids are all
+# just 0, 1, 2, ... n, its stored as an array
+# Ex: [ 5, 5, 7, 1, 1, 1 ] means units 0, 1 are in district 5, unit 2 is in
+# district 7, and units 3, 4, 5 are all in district 1
+
+# A border of a cell id is a tuple of (neighbor_id, border_length)
+
+# A district is a Set of ids (integers)
+
 
 def print_solution(solution):
     for y in range(h):
@@ -37,12 +45,13 @@ def get_cell_borders(i):
         borders.add((toI(x, y + 1), 1))
     return borders
 
+# Map of cell to set of (neighbor_id, border_length)s for each neighbor
 borders = [get_cell_borders(i) for i in range(w*h)]
 
-def get_perimeter(cells): # district: list of indices
+def get_perimeter(district):
     base = Set()
     perimeter = 0
-    for i in cells:
+    for i in district:
         for (neighbor, length) in borders[i]:
             if neighbor in base:
                 perimeter -= length
@@ -54,10 +63,9 @@ def get_perimeter(cells): # district: list of indices
 def get_area(cells):
     return reduce((lambda x, y: x + areas[y]), cells, 0)
 
-
-# Map of district number -> set of cells
+# Array of sets, index is district, value is a set of cell ids for that district
 def get_district_map(solution):
-    districts = [ Set() for d in range(0, num_districts + 1) ]
+    districts = [ Set() for d in range(0, num_districts) ]
     for i, d in enumerate(solution):
         districts[d].add(i)
     return filter(lambda s: len(s) != 0, districts)
@@ -67,49 +75,11 @@ def get_population(district):
 
 
 total_pop = reduce(lambda x, y: x + y, populations)
-def district_fitness(d):
-    compact = get_perimeter(d)**2 / float(get_area(d))
-    pop = abs(get_population(d) - (total_pop / float(num_districts)))
-    print('c: {}, p: {}, c + p: {}'.format(compact, pop, compact + pop))
-    return compact + pop
-
-
 def fitness(solution):
     districts = get_district_map(solution)
     d_compact = lambda d: get_perimeter(d)**2 / float(get_area(d))
     d_pop = lambda d: abs(get_population(d) - (total_pop / float(num_districts)))
-    return -reduce(lambda f, d: f + d_compact(d) + d_pop(d), districts, 0)
-
-
-# Converted from http://algs4.cs.princeton.edu/41graph/Bridge.java.html
-def get_bridges(district):
-    pre = {}
-    low = {}
-    for i in district:
-        pre[i] = -1
-        low[i] = -1
-
-    count = {'val': 0} # Hack to let me use the global variable in dfs
-    bridges = Set()
-    def dfs(u, v): # edge e = (u, v)
-        count['val'] += 1
-        pre[v] = count['val']
-        low[v] = pre[v]
-        district_borders = filter(lambda w: w[0] in district, borders[v])
-        neighbors = map(lambda w: w[0],  district_borders)
-        for w in neighbors:
-            if (pre[w] == -1):
-                dfs(v, w)
-                low[v] = min(low[v], low[w])
-                if (low[w] == pre[w]):
-                    bridges.add(v)
-            elif w != u:
-                low[v] = min(low[v], pre[w])
-
-    for cell in district:
-        dfs(cell, cell)
-
-    return bridges
+    return -reduce(lambda f, d: f + d_compact(d) + 2*d_pop(d), districts, 0)
 
 # Determine neighbors by moving a cell from one district to another
 # without causing any discontinuities
@@ -119,33 +89,96 @@ def solution_neighborhood(solution):
     for (cell, district) in enumerate(solution):
         if len(districts[district]) == 1:
             continue
-        bridges = get_bridges(districts[district])
-        if cell in bridges:
+        if not is_contiguous_without_cell(districts[district], cell):
             continue
         for (border_cell, _) in borders[cell]:
             if border_cell not in districts[district]:
+                # Swap cell to border_cell's district
                 neighbor = copy.deepcopy(solution)
                 neighbor[cell] = solution[border_cell]
                 neighborhood.append(neighbor)
     return neighborhood
 
+def is_contiguous_without_cell(district, cell_to_skip):
+    cells_to_check = set(district)
+    cells_to_check.remove(cell_to_skip)
+    random_cell = cells_to_check.pop()
+    def floodfill(cell):
+        if cell == cell_to_skip:
+            return
+        for (neighbor, _) in borders[cell]:
+            if neighbor in district:
+                if neighbor in cells_to_check:
+                    cells_to_check.remove(neighbor)
+                    floodfill(neighbor)
+    floodfill(random_cell)
+    return len(cells_to_check) == 0;
+
+
+    
+# Set the initial solution to just rows.  Note that if the number of districts
+# doesn't match the number of rows, this may not be a valid initial solution.
+# You might want to figure out a better way to generate a random valid initial
+# solution
+initial_solution = [int(i / (math.ceil((float(w*h))/num_districts))) for i in range(w*h)]
+initial_fitness = fitness(initial_solution)
 cur_solution = initial_solution
-cur_fitness = fitness(cur_solution)
-while True:
-    neighborhood = solution_neighborhood(cur_solution)
-    best = max(neighborhood, key=fitness)
-    best_fitness = fitness(best)
-    if best_fitness > cur_fitness:
-        cur_fitness = best_fitness
-        cur_solution = best
-    else:
+cur_fitness = initial_fitness 
+best_solution = cur_solution
+best_fitness = cur_fitness
+should_stop = False
+print(cur_fitness)
+
+# Simulated Annealing
+max_iterations = 10000
+initial_temperature = 10
+cooling_rate = 0.013
+temp_reduction = lambda temp: temp * (1 - cooling_rate)
+stopping_condition = lambda: should_stop or cur_temperature < 1
+iterations_per_temperature = 1
+
+cur_temperature = initial_temperature
+
+for iter in xrange(0, max_iterations):
+    for i in xrange(0, iterations_per_temperature):
+        neighborhood = solution_neighborhood(cur_solution)
+        if len(neighborhood) == 0:
+            should_stop = True
+            break
+
+        index = random.randint(0, len(neighborhood) - 1)
+        neighbor = neighborhood[index]
+        neighbor_fitness = fitness(neighbor)
+        change_in_fitness = neighbor_fitness - cur_fitness
+        if change_in_fitness > 0 or random.random() < math.exp(change_in_fitness / cur_temperature):
+            cur_solution = neighbor
+            cur_fitness = neighbor_fitness
+            if neighbor_fitness > best_fitness:
+                best_solution = neighbor
+                best_fitness = neighbor_fitness
+                print(best_fitness)
+
+    cur_temperature = temp_reduction(cur_temperature)
+
+    if stopping_condition():
         break
 
-#print('----------------------------------------')
-print('\n\n\n\n')
-print_solution(initial_solution)
-print('\n\n\n\n')
-#print('----------------------------------------')
-print_solution(cur_solution)
-#print('----------------------------------------')
 
+# # Hill Climbing
+# for iter in xrange(0, max_iterations):
+#     neighborhood = solution_neighborhood(cur_solution)
+#     local_best = max(neighborhood, key=fitness)
+#     local_best_fitness = fitness(best)
+#     if local_best_fitness > cur_fitness:
+#         cur_fitness = local_best_fitness 
+#         cur_solution = local_best
+#     else:
+#         break
+#     best_solution = cur_solution
+
+print('\n\n')
+print('-----  Initial (Cost: {})  -----'.format(-initial_fitness))
+print_solution(initial_solution)
+print('\n')
+print('-----   Final (Cost: {})   -----'.format(-best_fitness))
+print_solution(best_solution)
