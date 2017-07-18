6763044711818881ca86bfcf9b1a6530829c634f
David Zhang <davzee@hotmail.com>
2017-07-17 21:17:29 -0400

    Rewrote reader to read from census tract data

--- reader.py
+++ reader.py
@@ -1,59 +1,79 @@
 import shapefile
 import us
 
-STATEFIPS = 0
-GEOID = 2
-ALAND = 8
-AWATER = 9
+from termcolor import colored
 
-# STATE: [DISTRICT: AREA], AREA represented by km^2
-districts = {}
+AREA = 0
+PERIMETER = 1
+TRACT = 6
+
+# {STATE: {TRACT_DICTIONARY}}
+# TRACT_DICTIONARY MAPS TRACT_ID TO KEYS: AREA, PERIMETER, COORDINATES
+tracts = {}
 
 def print_fields(reader):
     for field in reader.fields:
         print field[0],
     print '\n'
 
-def process_data():
-    global districts
-    shp = open('shapefiles/tl_2016_us_cd115.shp', 'rb')
-    dbf = open('shapefiles/tl_2016_us_cd115.dbf', 'rb')
-    reader = shapefile.Reader(shp=shp, dbf =dbf)
+# Process shapefile data for a subset of states
+def process_data(*args, **kwargs):
+    if len(args) == 0:
+        print 'Provide a set of states as arguments to process_data().'
+        return
 
-    for record in reader.iterRecords():
-        state = get_state_identifier(record)
-        if state not in districts:
-            districts[state] = {}
-        geoid = get_geoid_identifier(record)
-        districts[state][geoid] = get_land_area(record) / float(10**6)
+    for state in args:
+        try:
+            fips = get_fips(state)
+            shp = open('shapefiles/tr%s_d00.shp' % (fips), 'rb')
+            dbf = open('shapefiles/tr%s_d00.dbf' % (fips), 'rb')
+            reader = shapefile.Reader(shp=shp, dbf =dbf)
 
-def print_areas(*args, **kwargs):
-    states = args if args else districts.keys()
+            if state not in tracts:
+                tracts[state] = {}
 
+            for shape in reader.shapeRecords():
+                tract_id = get_tract_id(shape.record)
+                if tract_id not in tracts[state]:
+                    tracts[state][tract_id] = {}
+
+                tracts[state][tract_id]['AREA'] = get_area(shape.record)
+                tracts[state][tract_id]['PERIMETER'] = get_perimeter(shape.record)
+                tracts[state][tract_id]['COORDINATES'] = shape.shape.points
+
+            shp.close()
+            dbf.close()
+        except IOError:
+            print ('Failed to read shapefile for %s' % (state))
+
+def get_tract_id(record):
+    return record[TRACT]
+
+def get_area(record):
+    return record[AREA]
+
+def get_perimeter(record):
+    return record[PERIMETER]
+
+def get_fips(state):
+    return us.states.lookup(unicode(state)).fips
+
+def print_tracts(*args, **kwargs):
+    states = args if len(args) > 0 else tracts.keys()
     for state in sorted(states):
-        print state
-        for geoid in sorted(districts[state].keys()):
-            print '\t- ' + geoid + ': ' + str(districts[state][geoid])
-        print_shapefiles(unicode(state))
-
-def get_land_area(record):
-    return record[ALAND]
-
-def get_state_identifier(record):
-    return us.states.lookup(record[STATEFIPS]).name
-
-def get_geoid_identifier(record):
-    return record[GEOID]
-
-def print_shapefiles(state):
-    urls = us.states.lookup(state).shapefile_urls()
-    for region, url in urls.items():
-        print '%s: %s' % (region, url)
-    print '\n'
+        print colored('State: %s' % (state), 'green')
+        _outer = tracts[state]
+        for tract_id in sorted(_outer.keys()):
+            _inner = _outer[tract_id]
+            print '\tTract: %s \n\t\t- Area: %s\n\t\t- Perimeter: %s' % (
+                tract_id,
+                _inner['AREA'],
+                _inner['PERIMETER']
+            )
 
 def main():
-    process_data()
-    print_areas('Iowa', 'North Carolina', 'South Carolina')
+    process_data('Iowa', 'North Carolina', 'South Carolina')
+    print_tracts('Iowa')
 
 if __name__ == '__main__':
     main()
