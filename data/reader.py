c39aae334c10e1c395528d1a9779063d5d3ff0b8
David Zhang <davzee@hotmail.com>
2017-07-18 00:39:25 -0400

    Add approximation on coordinates to reduce noise

--- data/reader.py
+++ data/reader.py
@@ -3,13 +3,18 @@ import us
 
 from termcolor import colored
 
+AREA_KEY = 'AREA'
+NAME_KEY = 'NAME'
+POPULATION_KEY = 'POPULATION'
+COORDINATES_KEY = 'COORDINATES'
+
 STATEFP = 0
 GEOID = 3
 NAME = 4
 ALAND = 13
 
 # {STATE: {COUNTY_DICTIONARY}}
-# COUNTY_DICTIONARY MAPS COUNTY GEOID TO KEYS: AREA, NAME, POPULATION
+# COUNTY_DICTIONARY MAPS COUNTY GEOID TO KEYS: AREA, NAME, POPULATION, COORDINATES, NEIGHBOURS
 _map = {}
 
 # MAPS COUNTY_NAME TO POPULATION
@@ -33,7 +38,7 @@ def process_data(*args, **kwargs):
 
         lines = pop.readlines()
         for line in lines:
-            name, population = line.split(', ')
+            name, population = line.strip().split(', ')
             _populations[name] = population
 
         pop.close()
@@ -46,8 +51,10 @@ def process_data(*args, **kwargs):
         dbf = open('files/tl_2016_us_county.dbf', 'rb')
         reader = shapefile.Reader(shp=shp, dbf =dbf)
 
-        for shape in reader.shapeRecords():
-            record = shape.record
+        for shape_record in reader.shapeRecords():
+            record = shape_record.record
+            shape = shape_record.shape
+
             state = get_state_name(record)
             if state not in states:     # State we are not interested in
                 continue
@@ -59,14 +66,20 @@ def process_data(*args, **kwargs):
             if county not in _map[state]:
                 _map[state][county] = {}
 
-            _map[state][county]['AREA'] = get_county_area(record)
-            _map[state][county]['NAME'] = get_county_name(record)
-            _map[state][county]['POPULATION'] = get_county_population(get_county_name(record))
+            _map[state][county][AREA_KEY] = get_county_area(record)
+            _map[state][county][NAME_KEY] = get_county_name(record)
+            _map[state][county][POPULATION_KEY] = get_county_population(get_county_name(record))
+
+            _map[state][county][COORDINATES_KEY] = approximate_coordinates(shape.points)
 
         shp.close()
         dbf.close()
     except IOError:
         print 'Failed to read shapefiles. Make sure to unzip files.zip first.'
+
+# Map coordinates to 1 decimal precision, then place in set to reduce noise
+def approximate_coordinates(points):
+    return list(set([(round(x[0], 1), round(x[1], 1)) for x in points]))
 
 def get_state_name(record):
     return us.states.lookup(record[STATEFP]).name
@@ -90,11 +103,12 @@ def print_mapping(*args, **kwargs):
         _outer = _map[state]
         for county in sorted(_outer.keys()):
             _inner = _outer[county]
-            print '\tCounty: %s \n\t\t- Name: %s\n\t\t- Area: %s\n\t\t- Population: %s' % (
+            print '\tCounty: %s \n\t\t- Name: %s\n\t\t- Area: %s\n\t\t- Population: %s\n\t\t- Coordinates: %s' % (
                 county,
-                _inner['NAME'],
-                _inner['AREA'],
-                _inner['POPULATION']
+                _inner[NAME_KEY],
+                _inner[AREA_KEY],
+                _inner[POPULATION_KEY],
+                _inner[COORDINATES_KEY]
             )
 
 def main():
