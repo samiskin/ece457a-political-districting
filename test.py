74fc4ca58e4fa8d5e50f73bc1273729c47f736f0
Leslie Reyes <ldianereyes@gmail.com>
2017-07-23 15:29:23 -0400

    add csv export to testing

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import simulated_annealing 
+import csv
+
+# @list: headers
+# @list of lists: data
+# @string: file_name
+def export_csv(headers, data, file_name):
+	with open(file_name, "wb") as output:
+	    writer = csv.writer(output, lineterminator='\n')
+	    writer.writerows([headers])
+	    writer.writerows(data)
 
 simulated_annealing.test(10, 0.003, 2)
