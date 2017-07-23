68f014604d64ba604f82589112161f227d9c5099
Ellen Wang <ellenwang95@yahoo.ca>
2017-07-23 17:04:19 -0400

    minor changes to test.py

--- test.py
+++ test.py
@@ -14,8 +14,22 @@ def export_csv(headers, data, file_name):
 	    writer.writerows([headers])
 	    writer.writerows(data)
 # @list: data
-def calculate_stats(data):
+def calculate_stats_helper(data):
+	print "min:", amin(data)
+	print "max:", amax(data)
+	print "median:", median(data)
+	print "stdev:", std(data)
 	return (amin(data), amax(data), median(data), std(data))
+
+def calculate_stats(comp_list, pop_list, fitness_list, diff_sum):
+	print "COMP:"
+	calculate_stats_helper(comp_list)
+	print "POP EQ:"
+	calculate_stats_helper(pop_list)
+	print "FITNESS:" 
+	calculate_stats_helper(fitness_list)
+	print "AVG DIFF IN FITNESS:"
+	print diff_sum/NUMBER_OF_RUNS
 
 def simulated_annealing_test():
 	comp_list = []
@@ -31,30 +45,7 @@ def simulated_annealing_test():
 		fitness_list.append(sa_best_fitness)
 		diff_sum += sa_best_fitness - sa_init_fitness
 
-	comp_stats = calculate_stats(comp_list)
-	print "COMP:"
-	print "min:", comp_stats[0]
-	print "max:", comp_stats[1]
-	print "median:", comp_stats[2]
-	print "stdev:", comp_stats[3]
-
-	pop_stats = calculate_stats(pop_list)
-	print "POP EQ:"
-	print "min:", pop_stats[0]
-	print "max:", pop_stats[1]
-	print "median:", pop_stats[2]
-	print "stdev:", pop_stats[3]
-
-	fitness_stats = calculate_stats(fitness_list)
-	print "FITNESS:" 
-	print "min:", fitness_stats[0]
-	print "max:", fitness_stats[1]
-	print "median:", fitness_stats[2]
-	print "stdev:", fitness_stats[3]
-
-	print "AVG DIFF IN FITNESS:"
-	print diff_sum/NUMBER_OF_RUNS
-
+	calculate_stats(comp_list, pop_list, fitness_list, diff_sum)
 
 def main():
 	simulated_annealing_test()
